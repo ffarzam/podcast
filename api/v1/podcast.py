39ce6c79f7cc35af58377cc7fa7395291cdf1665
unknown <ffarzam_1992@yahoo.com>
2023-11-10 02:18:44 +0330

    chore: update podcast endpoint

--- api/v1/podcast.py
+++ api/v1/podcast.py
@@ -4,11 +4,11 @@ from uuid import uuid4
 import pytz
 from fastapi import APIRouter, Depends, status, Request, Response
 
-from config.jwt_authentication import get_access_jwt_aut, AccessJWTBearer
+from config.jwt_authentication import get_access_jwt_aut
 from db.mongodb import get_podcast_database
 from schemas.interactions import Comment
 from services.django_microservice import get_django_client
-from utils.user import find_user, check_channel_bookmark, check_episode_like, check_episode_bookmark
+from utils.user import find_user, check_channel_bookmark, check_episode_like, check_episode_bookmark, get_users_list
 
 tz = pytz.timezone("Asia/Tehran")
 django_client = get_django_client()
@@ -17,9 +17,6 @@ routers = APIRouter(prefix="/v1")
 
 @routers.get("/channel_list/")
 async def channel_list(request: Request, response: Response, page: str | None = None, db=Depends(get_podcast_database)):
-    user_collection = db["users"]
-    podcast_collection = db["podcast"]
-
     django_microservice_response = await django_client.channel_list(request, page)
     response.status_code = django_microservice_response.status_code
     data = django_microservice_response.json()
@@ -83,8 +80,6 @@ async def get_item(request: Request, response: Response, channel_id: int, item_i
     django_microservice_response = await django_client.get_item(channel_id, item_id, request)
     response.status_code = django_microservice_response.status_code
     data = django_microservice_response.json()
-    print("1" * 100)
-    print(data)
     user_id = await find_user(request)
     if response.status_code == 200:
         refined_data = {k: v for k, v in data.items() if k not in ["seconds_listened"]}
@@ -101,29 +96,18 @@ async def get_item(request: Request, response: Response, channel_id: int, item_i
 @routers.get("/like/{channel_id}/{item_id}/", status_code=status.HTTP_200_OK)
 async def like(channel_id: int, item_id: int, db=Depends(get_podcast_database),
                payload: dict = Depends(get_access_jwt_aut())):
-    user_collection = db["users"]
-    podcast_collection = db["podcast"]
-
     user_id = payload["id"]
 
-    user_filtering = {"user_id": user_id}
-    channel_filtering = {"channel_id": channel_id}
-    # channel_filtering = {"channel_id": channel_id, "episode": {"$elemMatch": {"episode_id": item_id}}}
-
-    user_doc = {"$pull": {"liked": item_id}}
-    channel_doc = {"$pull": {f"episode.{item_id}.likes": user_id}}
-    # channel_doc = {"$pull": {f"episode.{item_id}.likes": user_id}}
-
-    data = await user_collection.update_one(filter=user_filtering, update=user_doc, upsert=True)
-    await podcast_collection.update_one(filter=channel_filtering, update=channel_doc, upsert=True)
+    data = await db["users"].update_one(filter={"user_id": user_id},
+                                        update={"$pull": {"liked": f"{channel_id}_{item_id}"}}, upsert=True)
+    await db["podcast"].update_one(filter={"channel_id": channel_id},
+                                   update={"$pull": {f"episode.{item_id}.likes": user_id}}, upsert=True)
 
     if data.modified_count == 0:
-        user_doc = {"$push": {"liked": item_id}}
-        channel_doc = {"$push": {f"episode.{item_id}.likes": user_id}}
-        # channel_doc = {"$push": {f"episode.{item_id}.likes": user_id}}
-
-        await user_collection.update_one(filter=user_filtering, update=user_doc, upsert=True)
-        await podcast_collection.update_one(filter=channel_filtering, update=channel_doc, upsert=True)
+        await db["users"].update_one(filter={"user_id": user_id},
+                                     update={"$push": {"liked": f"{channel_id}_{item_id}"}}, upsert=True)
+        await db["podcast"].update_one(filter={"channel_id": channel_id},
+                                       update={"$push": {f"episode.{item_id}.likes": user_id}}, upsert=True)
 
         return {"liked done"}
 
@@ -134,26 +118,18 @@ async def like(channel_id: int, item_id: int, db=Depends(get_podcast_database),
 async def bookmark_channel(channel_id: int,
                            db=Depends(get_podcast_database),
                            payload: dict = Depends(get_access_jwt_aut())):
-    user_collection = db["users"]
-    podcast_collection = db["podcast"]
-
     user_id = payload["id"]
 
-    user_filtering = {"user_id": user_id}
-    channel_filtering = {"channel_id": channel_id}
-
-    user_doc = {"$pull": {"channel_bookmark": channel_id}}
-    channel_doc = {"$pull": {"channel_bookmark": user_id}}
-
-    data = await user_collection.update_one(filter=user_filtering, update=user_doc, upsert=True)
-    await podcast_collection.update_one(filter=channel_filtering, update=channel_doc, upsert=True)
+    data = await db["users"].update_one(filter={"user_id": user_id}, update={"$pull": {"channel_bookmark": channel_id}},
+                                        upsert=True)
+    await db["podcast"].update_one(filter={"channel_id": channel_id}, update={"$pull": {"channel_bookmark": user_id}},
+                                   upsert=True)
 
     if data.modified_count == 0:
-        user_doc = {"$push": {"channel_bookmark": channel_id}}
-        channel_doc = {"$push": {"channel_bookmark": user_id}}
-
-        await user_collection.update_one(filter=user_filtering, update=user_doc, upsert=True)
-        await podcast_collection.update_one(filter=channel_filtering, update=channel_doc, upsert=True)
+        await db["users"].update_one(filter={"user_id": user_id}, update={"$push": {"channel_bookmark": channel_id}},
+                                     upsert=True)
+        await db["podcast"].update_one(filter={"channel_id": channel_id},
+                                       update={"$push": {"channel_bookmark": user_id}}, upsert=True)
         return {"bookmark done"}
 
     return {"bookmark undone"}
@@ -163,26 +139,22 @@ async def bookmark_channel(channel_id: int,
 async def bookmark_episode(channel_id: int, item_id: int,
                            db=Depends(get_podcast_database),
                            payload: dict = Depends(get_access_jwt_aut())):
-    user_collection = db["users"]
-    podcast_collection = db["podcast"]
-
     user_id = payload["id"]
 
-    user_filtering = {"user_id": user_id}
-    channel_filtering = {"channel_id": channel_id}
-
-    user_doc = {"$pull": {"episode_bookmark": item_id}}
-    channel_doc = {"$pull": {f"episode.{item_id}.bookmarks": user_id}}
-
-    data = await user_collection.update_one(filter=user_filtering, update=user_doc, upsert=True)
-    await podcast_collection.update_one(filter=channel_filtering, update=channel_doc, upsert=True)
+    data = await db["users"].update_one(filter={"user_id": user_id},
+                                        update={"$pull": {"episode_bookmark": f"{channel_id}_{item_id}"}},
+                                        upsert=True)
+    await db["podcast"].update_one(filter={"channel_id": channel_id},
+                                   update={"$pull": {f"episode.{item_id}.bookmarks": user_id}},
+                                   upsert=True)
 
     if data.modified_count == 0:
-        user_doc = {"$push": {"episode_bookmark": item_id}}
-        channel_doc = {"$push": {f"episode.{item_id}.bookmarks": user_id}}
-
-        await user_collection.update_one(filter=user_filtering, update=user_doc, upsert=True)
-        await podcast_collection.update_one(filter=channel_filtering, update=channel_doc, upsert=True)
+        await db["users"].update_one(filter={"user_id": user_id},
+                                     update={"$push": {"episode_bookmark": f"{channel_id}_{item_id}"}},
+                                     upsert=True)
+        await db["podcast"].update_one(filter={"channel_id": channel_id},
+                                       update={"$push": {f"episode.{item_id}.bookmarks": user_id}},
+                                       upsert=True)
         return {"bookmark done"}
 
     return {"bookmark undone"}
@@ -192,21 +164,17 @@ async def bookmark_episode(channel_id: int, item_id: int,
 async def create_comment(channel_id: int, item_id: int, comment: Comment,
                          db=Depends(get_podcast_database),
                          payload: dict = Depends(get_access_jwt_aut())):
-    user_collection = db["users"]
-    podcast_collection = db["podcast"]
-
     user_id = payload["id"]
     user_email = payload["email"]
 
-    user_filtering = {"user_id": user_id}
-    channel_filtering = {"channel_id": channel_id}
     d = datetime.strptime(datetime.now(tz).isoformat(), "%Y-%m-%dT%H:%M:%S.%f+03:30")
+
     user_doc = {"$push": {"comment": {f"{item_id}": [d, comment.content, 0, 0]}}}
     channel_doc = {
         "$push": {f"episode.{item_id}.comment": {f'{uuid4().hex}': [f"{user_email}", d, comment.content, 0, 0]}}}
 
-    await user_collection.update_one(filter=user_filtering, update=user_doc, upsert=True)
-    await podcast_collection.update_one(filter=channel_filtering, update=channel_doc, upsert=True)
+    await db["users"].update_one(filter={"user_id": user_id}, update=user_doc, upsert=True)
+    await db["podcast"].update_one(filter={"channel_id": channel_id}, update=channel_doc, upsert=True)
     return {"comment created"}
 
 
@@ -222,4 +190,31 @@ async def comments_list(channel_id: int, item_id: int,
     return {"no comments"}
 
 
+@routers.get("/users_bookmarked_channel/{channel_id}/", status_code=status.HTTP_200_OK)
+async def users_bookmarked_channel(channel_id: str, db=Depends(get_podcast_database)):
+    user_list = await get_users_list(int(channel_id), db)
+    return user_list
 
+
+@routers.get("/user_liked_episode_list/", status_code=status.HTTP_200_OK)
+async def user_liked_episode_list(db=Depends(get_podcast_database),
+                                  payload: dict = Depends(get_access_jwt_aut())):
+    user_id = payload["id"]
+    data = await db["users"].find_one({"user_id": user_id}, {"_id": 0, "liked": 1})
+    return data
+
+
+@routers.get("/user_channel_bookmark_list/", status_code=status.HTTP_200_OK)
+async def user_channel_bookmark_list(db=Depends(get_podcast_database),
+                                     payload: dict = Depends(get_access_jwt_aut())):
+    user_id = payload["id"]
+    data = await db["users"].find_one({"user_id": user_id}, {"_id": 0, "channel_bookmark": 1})
+    return data
+
+
+@routers.get("/user_episode_bookmark_list/", status_code=status.HTTP_200_OK)
+async def user_episode_bookmark_list(db=Depends(get_podcast_database),
+                                     payload: dict = Depends(get_access_jwt_aut())):
+    user_id = payload["id"]
+    data = await db["users"].find_one({"user_id": user_id}, {"_id": 0, "episode_bookmark": 1})
+    return data
